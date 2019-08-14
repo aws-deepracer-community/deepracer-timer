d929c931f6fb2091c157ab53fd40716a47fd8cb5
nalbam <me@nalbam.com>
2019-08-14 16:34:51 +0900

    pressure

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 
 app.get('/call/:name', function (req, res) {
     const name = req.params.name;
-    io.sockets.emit(`${name}`, (Math.random() * 100000));
+    io.sockets.emit(`${name}`, '0');
     return res.status(200).json({result : true});
 });
 
@@ -55,6 +55,7 @@ http.listen(port, function () {
 
 // // gpio
 // gpio.on('change', function(channel, value) {
+//     io.sockets.emit('pressure', value);
 //     console.log('Channel ' + channel + ' value is now ' + value);
 // });
 // gpio.setup(channel, gpio.DIR_IN, gpio.EDGE_BOTH);

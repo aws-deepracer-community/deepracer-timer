0e5392e44e1a5ebc6b0328e16513c550572048f1
nalbam <me@nalbam.com>
2019-08-16 21:05:04 +0900

    4

--- rpi/pressure.py
+++ rpi/pressure.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
 
 # initialise a previous input variable to 0 (Assume no pressure applied)
 prev_input = 0
@@ -10,7 +10,7 @@ prev_input = 0
 try:
     while True:
         # take a reading
-        input = GPIO.input(7)
+        input = GPIO.input(4)
         print(input)
 
         # if the last reading was low and this one high, alert us

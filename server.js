6d50a669a7d2e125714c59176a8435595d67fd38
nalbam <me@nalbam.com>
2019-08-28 00:42:28 +0900

    tab

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ http.listen(port, function () {
 
 // gpio
 gpio.on('change', function (channel, value) {
-    console.log(`Channel ${channel} value is now ${value} - ${(Math.random() * 100000)}`);
+    console.log(`Channel ${channel} value is now ${value} \t- ${(Math.random() * 100000)}`);
     if (channel === 7) {
         io.sockets.emit('call', 'press');
     } else if (channel === 37) {
